4f86af6415f14ab65f3aba51e2c67705ee7c9f25
Jonathan Gropper <JonnyGropper@Jonnys-MacBook-Pro.local>
2016-11-10 08:44:00 -0500

    Updated Twitter a

--- twitterhw3a.py
+++ twitterhw3a.py
@@ -4,5 +4,33 @@
 
 # You will demo this live for grading.
 
+import tweepy
+
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+def main():
+  # Fill in the values noted in previous step here
+  cfg = { 
+    "consumer_key"        : "hvhMYZ99LcHrClJIQBI0kE6Gf",
+    "consumer_secret"     : "xjhLHGUwwMHeQlfBFi9G8A5gYS29cQKX0KOo0ulHjXDjw6jXHM",
+    "access_token"        : "242105174-V8UMay30MTfwhITBCpslZkxy1qHCwG5eiKJngTlG",
+    "access_token_secret" : "TktnkmDXa07vYOWZHNq9Faa04r8LwJLKfgZMJtvXP3txY" 
+    }
+
+  api = get_api(cfg)
+  #tweet = '/mnt/sda1/moneyplant.png'
+  #status = api.update_with_media(image_path, tweet)
+  # Yes, tweet is called 'status' rather confusing
+  tweet = "#UMSI-206 #Proj3"
+  status = api.update_with_media("smiley-face.jpg", tweet)
+
+if __name__ == "__main__":
+  main()
+
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
+
+
